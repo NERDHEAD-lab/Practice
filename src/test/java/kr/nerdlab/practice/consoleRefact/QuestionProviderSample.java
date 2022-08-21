04b2a91578d3e11f23c183c6a43cb6fa836e0c73
류주성 <juseong.ryu@epapyrus.com>
2022-08-21 14:26:44 +0900

    feat: Console에서 사용하는 질의 및 선택 목록에서 받을 값에 대한 코드 리뷰 to 이상빈

---
+++ src/test/java/kr/nerdlab/practice/consoleRefact/QuestionProviderSample.java
@@ -0,0 +1,80 @@
+package kr.nerdlab.practice.consoleRefact;
+
+import kr.nerdlab.practice.BaseSample;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class QuestionProviderSample extends BaseSample {
+
+	public static void main(String[] args) {
+		new QuestionProviderSample().reviewCode();
+	}
+
+	@Override
+	public void originalCode() {
+		System.out.println("배팅 할 곳을 선택하세요.");
+		System.out.println("Player - 1");
+		System.out.println("Banker - 2");
+		Scanner st = new Scanner(System.in);
+		// ...
+		int i = 0;
+		while (i == 0) {
+			System.out.println("계속 하시겠습니까?");
+			System.out.println("Yes - 1");
+			System.out.println("No  - 2");
+			Scanner sc = new Scanner(System.in);
+			int stop = sc.nextInt();
+			if (stop == 1) {
+				//game.play(gamer);
+			} else {
+				System.out.println("게임을 종료합니다.");
+				i++;
+			}
+		}
+	}
+
+
+	@Test
+	@Override
+	public void reviewCode() {
+//		QuestionProvider provider = QuestionProvider.of(
+//				"계속 하시겠습니까?",
+//				answers ->
+//						answers.answer("Yes", () -> System.out.println("게임을 종료합니다."))
+//								.answer("No", () -> System.out.println("게임을 종료합니다."))
+//		);
+
+
+//Case 1
+		QuestionProvider.of(
+				"질문입니다!",
+				answers
+						-> answers
+							.answer("나는 바보다", () -> System.out.println("바보 ㅇㅈㅇㅈ"))
+							.answer("나는 바보가 아닌디유", () -> System.out.println("엥 바보 맞는디유"))
+		).run();
+//Case 2
+		int i1 = QuestionProvider.of(
+				"질문입니다!",
+				answers
+						-> answers
+						.answer("나는 바보다", () -> System.out.println("바보 ㅇㅈㅇㅈ"))
+						.answer("나는 바보가 아닌디유", () -> System.out.println("엥 바보 맞는디유"))
+		).runWithResult();
+//Case 3
+		QuestionProvider provider = QuestionProvider.of(
+				"질문입니다!",
+				answers
+						-> answers
+						.answer("나는 바보다", () -> System.out.println("바보 ㅇㅈㅇㅈ"))
+						.answer("나는 바보가 아닌디유", () -> System.out.println("엥 바보 맞는디유"))
+		);
+		//logger.info("provider instance init finished");
+
+		int i = provider.runWithResult();
+//		int result = provider.runWithResult();
+	}
+}
