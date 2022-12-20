468b06c79efe1dde5f6a4dacd1f04a649a72d706
류주성 <juseong.ryu@epapyrus.com>
2022-12-20 16:06:29 +0900

    chore: 테스트 코드 추가

---
+++ src/test/java/kr/nerdlab/practice/parser/NotStringTest.java
@@ -0,0 +1,37 @@
+package kr.nerdlab.practice.parser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+
+public class NotStringTest {
+	private static final Logger logger = LoggerFactory.getLogger(NotStringTest.class);
+
+	@Test
+	public void notStringWithMap() {
+		String source = "hello ${data} and $data";
+		NotString<String> nStr = new DefaultNotString(source, new HashMap<>() {{
+			put("data", "world");
+		}});
+
+		Assert.assertEquals(nStr.toString(), source.replaceAll("\\$\\{data}", "world"));
+	}
+
+	@Test
+	public void notStringWithMapAndConfig() {
+		String source = "hello ${{data}} and ${data}";
+		NotString<String> nStr = new DefaultNotString(source, new HashMap<>() {{
+			put("data", "world");
+		}});
+		nStr.setConfig(
+				NotConfig.create(new NotTypeReference<String>() {
+						})
+						.surroundWith("${{", "}}")
+		);
+
+		Assert.assertEquals(nStr.toString(), source.replaceAll("\\$\\{\\{data}}", "world"));
+	}
+}
